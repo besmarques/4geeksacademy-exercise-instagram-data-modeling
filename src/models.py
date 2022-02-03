c2e34bdf456e12cf4681d505c46e10dace76ae93
Bruno Marques <besmarques@gmail.com>
2022-02-03 13:20:09 +0000

    work started

--- src/models.py
+++ src/models.py
@@ -8,23 +8,68 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+#class Person(Base):
+#    __tablename__ = 'persondsasdasd'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+#    id = Column(Integer, primary_key=True)
+#    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+#class Address(Base):
+#    __tablename__ = 'address'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
+#    id = Column(Integer, primary_key=True)
+#    street_name = Column(String(250))
+#    street_number = Column(String(250))
+#    post_code = Column(String(250), nullable=False)
+#    person_id = Column(Integer, ForeignKey('person.id'))
+#    person = relationship(Person)
+
+class User(Base):
+    __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    username = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    firstname = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    posts_id = Column(Integer, ForeignKey('posts.id'))
+    posts = relationship(Posts)
+
+class Followers(Base):
+    __tablename__ = 'follow'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    following_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Sugestions(Base):
+    __tablename__ = 'sugestions'
+    id = Column(Integer, primary_key=True)
+    sugestion = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class Posts(Base):
+    __tablename__ = 'posts'
+    id = Column(Integer, primary_key=True)
+    date_posted = Column(String(250), nullable=False)
+    local = Column(String, ForeignKey('local.id'))
+    local = relationship(Local)
+
+class Comments(Base):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    text = Column(String, ForeignKey('local.id'))
+    author = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post = relationship(Posts)
+
+
+
+
+
 
     def to_dict(self):
         return {}
